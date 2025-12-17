660de2ff9afc01b85438d6384bf98e522e53968c
{-Ovidiu Sandu-} <{-ovy79ro@yahoo.com-}>
2025-12-17 21:23:19 +0200

    add layout page

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,7 @@
-export default function Home(){
+export default function Home({name}){
     return (
         <>
-            <h1 className="title">Hello user</h1>
+            <h1 className="title">Hello {name}</h1>
         </>
     )
 }
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return inertia('Home');
+    return inertia('Home', [
+        'name' => 'Ovidiu'
+    ]);
 });
