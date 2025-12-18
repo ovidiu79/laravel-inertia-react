116e2c19e8ce3c03675f849edf310ffe44416628
{-Ovidiu Sandu-} <{-ovy79ro@yahoo.com-}>
2025-12-18 18:52:44 +0200

    add pagination

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return inertia('Home', [
-        'name' => 'Ovidiu'
-    ]);
-});
+Route::get('/', [PostController::class, 'index']);
+
+Route::resource('posts', PostController::class)->except('index');
